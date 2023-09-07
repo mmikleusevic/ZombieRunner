10f91ccb5e0e744abdd0b09a34186d33963b4845
mikleusevicmarin <mikleusevicmarin@gmail.com>
2023-09-07 13:00:20 +0200

    added new weapons, tuned them

--- Assets/Weapons/Weapon.cs
+++ Assets/Weapons/Weapon.cs
@@ -11,28 +11,34 @@ public class Weapon : MonoBehaviour
     [SerializeField] float _damage = 30f;
     [SerializeField] GameObject _hitVFX;
     [SerializeField] Ammo _ammoSlot;
+    [SerializeField] float _weaponCooldownTimer = 0.2f;
 
-    float _weaponCooldownTimer = COOLDOWN_TIME;
-
-    static float COOLDOWN_TIME = 0.1f;
+    bool _canShoot = true;
 
     private void Update()
     {
         bool isPressed = _fire.ReadValue<float>() > 0.5 ;
 
-        if (isPressed && _weaponCooldownTimer <= 0 && _ammoSlot.GetCurrentAmmo() > 0)
+        if (isPressed && _canShoot)
         {
-            Shoot();
+            StartCoroutine(Shoot());
         }
-
-        _weaponCooldownTimer -= Time.deltaTime;
     }
 
-    private void Shoot()
+    private IEnumerator Shoot()
     {
-        PlayMuzzleFlash();
-        ProcessRaycast();
-        ReduceAmmo();
+        _canShoot = false;
+
+        if(_ammoSlot.GetCurrentAmmo() > 0)
+        {
+            PlayMuzzleFlash();
+            ProcessRaycast();
+            ReduceAmmo();
+        }
+
+        yield return new WaitForSeconds(_weaponCooldownTimer);
+
+        _canShoot = true;
     }
 
     private void PlayMuzzleFlash()
@@ -42,8 +48,6 @@ public class Weapon : MonoBehaviour
 
     private void ProcessRaycast()
     {
-        _weaponCooldownTimer = COOLDOWN_TIME;
-
         RaycastHit hit;
         if (Physics.Raycast(_FPCamera.transform.position, _FPCamera.transform.forward, out hit, _range))
         {
